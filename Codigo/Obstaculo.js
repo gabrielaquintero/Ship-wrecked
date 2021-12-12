e430ff50d82933896f99dfb2bdc154b4affb69cf
unknown <isabelitaferrer01@live.com>
2021-12-11 21:11:25 -0500

    ultimo avance

--- Codigo/Obstaculo.js
+++ Codigo/Obstaculo.js
@@ -1,20 +1,15 @@
-class Coleccionable {
+class Obstaculo {
     constructor(x,y,tamaño) {
         this.x = x;
         this.y = y;
         this.x1 = x + tamaño;
         this.y1 = y + tamaño;
         this.tamaño = tamaño;
-        this.coleccionado = false;
     }
 
-    dibujar(imagen) {
-        if (this.coleccionado === false) {
+        dibujar(imagen) {
             fill(this.x, this.y, this.tamaño);
             image(imagen, this.x, this.y, this.tamaño, this.tamaño);
-        } else { 
-            console.log(this.coleccionado)
-        }
     }
     
     getPos() {
@@ -22,12 +17,6 @@ class Coleccionable {
         posiciones.push(this.x, this.y, this.x1, this.y1);
         return posiciones;
     }
-
-    setColeccionado(valor) { 
-        this.coleccionado = valor;
     }
 
-    getColeccionado() { 
-        return this.coleccionado;
-    }
-    }
+    
